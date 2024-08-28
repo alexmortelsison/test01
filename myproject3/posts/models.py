59926d5717b04b3abc6611df432bdb5ddb60362f
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-29 08:37:26 +0900

    updated

--- myproject3/posts/models.py
+++ myproject3/posts/models.py
@@ -4,7 +4,7 @@ class Post(models.Model):
     title = models.CharField(max_length=75)
     body = models.TextField()
     slug = models.SlugField()
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
